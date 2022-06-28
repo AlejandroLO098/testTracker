1a5eba239b06fad08a5783d47ea823ffda54ad02
Alejandro Lopez <alejandrolopez@MacBook-Pro-4.local>
2022-06-27 22:20:03 -0400

    sigh

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "devuser",
   password: "OBIwan099*",
-  database: "ToDoList",
+  database: "TaskTracker",
 });
 
 connection.connect((err) => {
@@ -21,3 +21,63 @@ connection.connect((err) => {
 app.listen(3306, () => {
   console.log(`listening on port`);
 });
+class DbService {
+  static getDbServiceInstance() {
+    return instance ? instance : new DbService();
+  }
+
+  async getAllData() {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = "SELECT * FROM tasks";
+
+        connection.query(query, (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results);
+        });
+      });
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async insertNewName(name) {
+    try {
+      const dateAdded = new Date();
+      const insertId = await new Promise((resolve, reject) => {
+        const query = "INSERT INTO tasks (task_text) VALUES ?;";
+
+        connection.query(query, [name, dateAdded], (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results);
+        });
+      });
+      console.log(insertId);
+      return insertId;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async updateNameById(id, name) {
+    try {
+      id = parseInt(id, 10);
+      const response = await new Promise((resolve, reject) => {
+        const query = "UPDATE tasks SET task_text = ? WHERE id = ?";
+
+        connection.query(query, [name, id], (err, result) => {
+          if (err) reject(new Error(err.message));
+          resolve(result.affectedRows);
+        });
+      });
+
+      return response === 1 ? true : false;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+}
+
+module.exports = DbService;
